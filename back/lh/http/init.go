9c9b6ff5804680b3faa045952e270f6f5db8ab2a
xodnr8520@naver.com <xodnr8520@naver.com>
2022-04-10 19:18:49 +0900

    update 수정

--- back/lh/http/init.go
+++ back/lh/http/init.go
@@ -138,10 +138,10 @@ func uploadRightdocProcedure(name, id, docname string, Body interface{}) *Proble
 	key := fmt.Sprintf("%s-%s", name, id)
 	fmt.Println(key, "   ", id)
 	savepath := fmt.Sprintf("%s/%s/%s", file_path, key, docname)
-	file, err := os.OpenFile(savepath, os.O_CREATE|os.O_RDWR, os.FileMode(0644))
+	file, err := os.OpenFile(savepath, os.O_CREATE|os.O_RDWR, os.FileMode(777))
 	if strings.Contains(err.Error(), "no such file or directory") {
-		os.MkdirAll(fmt.Sprintf("%s/%s", file_path, key), os.FileMode(0644))
-		file, err = os.OpenFile(savepath, os.O_CREATE|os.O_RDWR, os.FileMode(0644))
+		os.MkdirAll(fmt.Sprintf("%s/%s", file_path, key), os.FileMode(777))
+		file, err = os.OpenFile(savepath, os.O_CREATE|os.O_RDWR, os.FileMode(777))
 	}
 	if err != nil {
 		return &ProblemDetails{
