740925da660a371fa976e62db64f8c75b7ab8a83
xodnr8520@naver.com <xodnr8520@naver.com>
2022-04-11 20:59:53 +0900

    env 설정 test

--- back/lh/main.go
+++ back/lh/main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"lh/fabric"
 	"lh/http"
 	"os"
 )
 
 func main() {
-	os.Setenv("TESTTEST", "TRUE")
+	os.Setenv("TESTTEST", "XX")
+	
+	fmt.Println(os.Getenv("TESTTEST"))
 	//DISCOVERY_AS_LOCALHOST=TRUE  필수[env]
 	os.Setenv("DISCOVERY_AS_LOCALHOST", "TRUE")
 	fabric.Init_Wallet()
